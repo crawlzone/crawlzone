69af8b309bc764a904137c02bc50619d20e7b548
zstate <zstate@gmail.com>
2019-05-06 11:01:06 -0400

    Checking if node is installed.

--- src/Handler/PuppeteerHandler.php
+++ src/Handler/PuppeteerHandler.php
@@ -10,6 +10,7 @@ use GuzzleHttp\Promise\PromiseInterface;
 use GuzzleHttp\Promise\RejectedPromise;
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\RequestInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 class PuppeteerHandler implements Handler
@@ -19,6 +20,8 @@ class PuppeteerHandler implements Handler
 
     /**  @var Process */
     private $processes;
+
+    private $isNodeInstalled;
 
     /**
      * @return void
@@ -63,11 +66,12 @@ class PuppeteerHandler implements Handler
             'uri' => (string) $request->getUri()
         ]);
 
-        //@todo: check if node can be run, throw exception otherwise
+        $this->guardNodeJsInstallation();
+
         //@todo: check if the puppeteer library is installed
         $cmd = "node " . __DIR__ . "/../browser.js " . escapeshellarg($arguments);
 
-        $process = new Process($cmd);
+        $process = Process::fromShellCommandline($cmd);
 
         $pid = spl_object_hash($process);
 
@@ -78,5 +82,23 @@ class PuppeteerHandler implements Handler
         $this->promises[$pid] = new Promise([$this,'execute']);
 
         return $this->promises[$pid];
+    }
+
+    private function guardNodeJsInstallation(): void
+    {
+        if($this->isNodeInstalled) {
+            return;
+        }
+
+        $process = Process::fromShellCommandline("node -v");
+        $process->run();
+
+        if(! $process->isSuccessful()) {
+            throw new \RuntimeException("Please install Node.js - https://nodejs.org/");
+        }
+
+        $this->isNodeInstalled = true;
+
+        return;
     }
 }
